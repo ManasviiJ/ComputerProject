c28107ec105308a4ec2ccbf89ff1174195fef270
ManasviiJ <manasvij.mahi@gmail.com>
2024-10-27 12:34:04 +0530

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -5,6 +5,7 @@ subheader(":rainbow[Build Better Habits, Build a Better Life]")
 write("What would you like to do today?")
 
 page_link("pages/Habits.py", label="Habits", icon="🌳")
+page_link.("pages/To_Do_List.py",label="To Do List", icon="✔")
 
 divider()
 page_link("pages/About_us.py", label=":grey[About us]", icon="❤")
