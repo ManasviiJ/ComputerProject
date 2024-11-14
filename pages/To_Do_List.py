af8fc6a7d3ede23c6d38d8184f9b319e46ad3f2c
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-14 22:55:30 +0530

    Update To_Do_List.py

--- pages/To_Do_List.py
+++ pages/To_Do_List.py
@@ -4,7 +4,7 @@ page_link("Welcome_Page.py",label="",icon="🔙")
 if 'task_list' not in session_state:
   session_state.task_list=[]
 task=text_input("Enter new task")
-session_state.task_list.append('task')
+session_state.task_list.append(task)
 for i in session_state.task_list:
   if checkbox(i):
     balloons()
