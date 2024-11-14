a4757a48586aba900e97fa6c5ede1c89a577c4e1
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-14 22:37:54 +0530

    Update Habits_Scorecard.py

--- pages/Habits_Scorecard.py
+++ pages/Habits_Scorecard.py
@@ -9,7 +9,7 @@ if 'entry' not in session_state:
 title("Habits Scorecard")
 entry=text_input("Make a list of your daily habits")
 session_state.entry_list.append(entry)
-for i in entry_list:
+for i in session_state.entry_list:
   checkbox(i)
 
 
