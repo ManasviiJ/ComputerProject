1cb18fb53fa1d93da0dfaea9598485f4eae9e7c6
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-14 22:28:51 +0530

    Update Habits_Scorecard.py

--- pages/Habits_Scorecard.py
+++ pages/Habits_Scorecard.py
@@ -1,8 +1,13 @@
 from streamlit import *
+import time
 
 title("Habits Scorecard")
 entry=text_input("Make a list of your daily habits")
 
-def tutorial():
-  return
-button("How does this work?",command=tutorial)
+
+divider()
+if button("How does this work?"):
+  toast("Habit scorecard is a simple excercise you can use to become more aware of your behaviour")
+  time.sleep(1)
+  toast("You can create a list of your daily habits to keep track of them and review them as you cut down bad habits and replace them with good ones")
+  
