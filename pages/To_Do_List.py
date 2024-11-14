622d7b392566775dffd76cdab88b9ed7883bdf8d
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-14 22:53:31 +0530

    Update To_Do_List.py

--- pages/To_Do_List.py
+++ pages/To_Do_List.py
@@ -1,3 +1,10 @@
 from streamlit import *
 page_link("Welcome_Page.py",label="",icon="🔙")
 
+if 'task_list' not in session_state:
+  session_state.task_list=[]
+task=text_input("Enter new task")
+sessrion_state.task_list.append('task')
+for i in session_state.task_list:
+  if checkbox(i):
+    balloon()
