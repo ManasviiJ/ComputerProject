4640552b08d957b25f1a4a288fd496ab9f5d6fc6
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-16 00:39:02 +0530

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -1,6 +1,26 @@
 from streamlit import*
 page_link("Welcome_Page.py",label="",icon="🔙")
 
+mot_list=["You do not rise to the level of your goals. You fall to the level of your systems.",
+"Habits are the compound interest of self-improvement.",
+"Success is the product of daily habits—not once-in-a-lifetime transformations.",
+"The most effective way to change your habits is to focus not on what you want to achieve, but on who you wish to become.",
+"You do not need to be radical in your approach to change; small changes can have a powerful impact.",
+"Every action you take is a vote for the type of person you wish to become.",
+"The key to building lasting habits is focusing on creating a new identity first.",
+"A habit must be established before it can be improved.",
+"Small habits make a big difference in the long run.",
+"The truth is, our habits are a reflection of our identity.",
+"The process of building habits is like remodeling a house: it’s a lot of work up front, but the payoff is a more beautiful and functional life."
+"You get what you repeat.",
+"The most important thing is to keep going.",
+"Make it easy. Make it obvious. Make it satisfying.",
+"Good habits make time your ally. Bad habits make time your enemy.",
+"The secret to getting results that last is to never stop making improvements.",
+"The greatest threat to success is not failure, but boredom.",
+"You do not need to be the best, you just need to be 1% better than yesterday.",
+"Master the fundamentals. Master the basics. Build a strong foundation."
+]
 title("Do you want to get motivated:)?")
 button("motivate me",icon="👍🏼")
 divider()
