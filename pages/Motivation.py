9cc12525a1d4bc6beb5dd7fb493ca2557ab8cc04
Browneyesimp <cnbr.8641@gmail.com>
2024-11-16 00:32:59 +0530

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -1,11 +1,12 @@
 from streamlit import*
 page_link("Welcome_Page.py",label="",icon="🔙")
+balloons()
 
 title("Do you want to get motivated:)?")
 button("motivate me",icon="👍🏼")
 divider()
 button("no thank you",icon="👎🏼")
-balloons()
+
 
 
 
