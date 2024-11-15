3e0bafc5abdb1b0847017009a2ac85023bd2594e
Browneyesimp <cnbr.8641@gmail.com>
2024-11-16 00:35:48 +0530

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -4,7 +4,7 @@ page_link("Welcome_Page.py",label="",icon="🔙")
 title("Do you want to get motivated:)?")
 button("motivate me",icon="👍🏼")
 divider()
-button("no thank you",icon="👎🏼")balloons()
+button("no thank you",icon="👎🏼")
 
 
 
