1c72405c93c339ba08bab17826843f427469349a
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-15 15:49:14 +0530

    Update Habits.py

--- pages/Habits.py
+++ pages/Habits.py
@@ -17,5 +17,6 @@ if  button('Add habit'):
   
 subheader('Your new habits:')
 for habit, days in session_state.habits.items():
-    write(habit,"for",days,"days")
+    if checkbox(habit,"for",days,"days"):
+        balloons()
 
