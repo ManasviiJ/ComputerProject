a646a03c26094dae2c2ee9eb99f08c2d5e68dad1
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-15 16:35:04 +0530

    Update Habits_Scorecard.py

--- pages/Habits_Scorecard.py
+++ pages/Habits_Scorecard.py
@@ -18,6 +18,6 @@ if button("How does this work?"):
   toast("Habit scorecard is a simple excercise you can use to become more aware of your behaviour")
   time.sleep(3)
   toast("You can create a list of your daily habits to keep track of them and review them as you cut down bad habits and replace them with good ones")
-    time.sleep(3)
-    toast("You can add new habits after certain habits yu do in a day to make it doable")
+ time.sleep(3)
+ toast("You can add new habits after certain habits yu do in a day to make it doable")
 
