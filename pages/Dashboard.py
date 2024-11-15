7834b9a7ca1acc9008da246dcf0ca70899f47d5b
Browneyesimp <cnbr.8641@gmail.com>
2024-11-15 22:30:21 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -2,7 +2,7 @@ from streamlit import *
 
 page_link("Welcome_Page.py",label="",icon="🔙")
 
-title(:blue["Dashboard"])
+title(":blue[Dashboard]")
 subheader("Your existing habits:")
 
 for habit,days in session_state.habits.items():
