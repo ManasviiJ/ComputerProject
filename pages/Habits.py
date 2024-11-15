1d92a3d1098a785b65a1125ac66938cf88dbd822
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-15 16:41:18 +0530

    Update Habits.py

--- pages/Habits.py
+++ pages/Habits.py
@@ -8,14 +8,12 @@ title(':green[Habits]')
 #Initializing session state to store habits as a dictionary
 if 'habits' not in session_state:
     session_state.habits={}
-if 'count' not in session_state:
-    session_state.count=0
    
 #Entering new habits and displaying them
 new_habit=text_input('Enter you new habit', label_visibility='collapsed')
 days_to_follow=select_slider('How  many ***days*** do you wish to follow your new habit?', options=range(31))
   
-if  button('Add habit'):
+if  button('Add habit') and new_habit:
     session_state.habits[new_habit]=days_to_follow
 
 def check():
