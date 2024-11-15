71dc6140cc0c00d069e79a3db220f2619cfd8cfc
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-16 00:12:39 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -8,11 +8,11 @@ write()
 col1,col2=columns(2)
 with col1:
   subheader(":grey[Your existing habits:]")
-  for habit,days in session_states.habits.items():
+  for habit,days in session_state.habits.items():
     write(habit,"for",days,"days")
 with col2:
   subheader("Incomplete tasks in to do list")
-  for task_not_done in session_states.tasks:
+  for task_not_done in session_state.tasks:
     if task_not_done['status']==False:
       write(task_not_done['task'])
 
