719dd0938a602abba9f10f1df4187a09194968fc
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-15 22:37:04 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -2,14 +2,10 @@ from streamlit import *
 
 page_link("Welcome_Page.py",label="",icon="🔙")
 
-col1, col2, col3=columns(3)
-with col2:
-  title(":violet[Dashboard]")
-with col1:
-  subheader(":grey[Your existing habits:]")
-  for habit,days in session_state.habits.items():
-    write(habit,"for",days,"days")
+title(":violet[Dashboard]")
+subheader(":grey[Your existing habits:]")
+for habit,days in session_state.habits.items():
+  write(habit,"for",days,"days")
 
 divider()
-with col3:
-  subheader("Your To-DO List for the day")
+subheader("Your To-DO List for the day")
