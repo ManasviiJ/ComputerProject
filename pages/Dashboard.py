942921f543ced1f8a9fa06de1d7ec4b36f577285
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-15 22:27:39 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,9 +1,14 @@
 from streamlit import *
+
 page_link("Welcome_Page.py",label="",icon="🔙")
+
 title("Dashboard")
 subheader("Your existing habits:")
+
 for habit,days in session_state.habits.items():
   write(habit,"for",days,"days")
+
 divider()
+
 subheader("Your To-DO List for the day")
 
