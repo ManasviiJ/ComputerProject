6509e5f7e7a07e4e38e95a998846b6a12b50f5af
ManasviiJ <manasvij.mahi@gmail.com>
2024-11-15 14:20:16 +0530

    Update About_us.py

--- pages/About_us.py
+++ pages/About_us.py
@@ -2,8 +2,22 @@ from streamlit import *
 
 page_link("Welcome_Page.py",label="",icon="🔙")
 
-title(":red[About us]")
-markdown('''Welcome to your personal pocket cheerleader! 🌟 Whether you're forming new habits or chasing your dreams, we're here to help you stay on track. With every step forward, big or small, you're building a better you—one day at a time. Let's grow together and make everyday wins feel extra special! 💪✨''')
+title(":red[Why choose us?]")
+markdown('''*Description:*
+
+HabitForge is a simple, yet powerful habit tracker app inspired by the principles of "Atomic Habits". Our app helps you create lasting changes by  by focusing on small incremental improvements
+That's correct!
+
+Daniel Kahneman, Nir Eyal, and Aubrey Marcus are indeed notable figures who have drawn inspiration from "Atomic Habits" by James Clear.
+
+*Daniel Kahneman*: Nobel Prize-winning economist and author of "Thinking, Fast and Slow". Kahneman's work on behavioral economics and cognitive biases laid the groundwork for Clear's habits-based approach.
+
+*Nir Eyal*: Author of "Hooked: How to Build Habit-Forming Products" and behavioral designer. Eyal's work focuses on designing engaging products that create lasting habits.
+
+*Aubrey Marcus*: Founder of Onnit, author, and podcaster. Marcus has integrated Atomic Habits principles into Onnit's wellness and self-improvement programs.
+ Habits"  and motivational 
+
+These thought leaders recognize the value of "Atomic Habits" in providing actionable strategies for personal growth, self-improvement, and behavioral change.''')
 caption('Your feedback is really important to us!')
 if feedback('stars'):
     write("Thank you for choosing us!")
