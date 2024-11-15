b9c9696da68e248b2daf534aaf7d9aa08c54827e
IreneAnnaJosy0309 <nehababy0309@gmail.com>
2024-11-16 00:07:50 +0530

    Update Motivation.py

--- pages/Motivation.py
+++ pages/Motivation.py
@@ -1,6 +1,7 @@
 from streamlit import*
-title("Do you want to get motivated:)?")
 page_link("Welcome_Page.py",label="",icon="🔙")
+
+title("Do you want to get motivated:)?")
 button("motivate me",icon="👍")
 
 
