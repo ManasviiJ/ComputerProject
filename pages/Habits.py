d406da1072a6e4aa06f80ef1c1c1ace984f8aa65
ManasviiJ <manasvij.mahi@gmail.com>
2025-01-01 15:57:44 +0530

    added message

--- pages/Habits.py
+++ pages/Habits.py
@@ -1,4 +1,5 @@
 from streamlit import *
+from streamlit import segmented_control
 
 page_link("Welcome_Page.py",label="",icon="🔙")
 
@@ -8,5 +9,19 @@ subheader("What would you like to do today?")
 col1,col2=columns(2)
 with col1:
     page_link("pages/progress.py",label="See your progress",icon="🚨")
+with col2:
     page_link("pages/addhabit.py",label="add a new habit",icon="🔥")
 
+subheader("Your current habits are:")
+options = ["Morning", "Afternoon", "Evening"]
+selection =segmented_control("Directions", options, selection_mode="multi")
+markdown(f"Your selected options: {selection}.")
+
+coli,colii,coliii=columns(3)
+with coli:
+    subheader('Morning')
+    text_area('Wake up at 8AM')
+with colii:
+    subheader('Afternoon')
+with coliii:
+    subheader('Evening')
