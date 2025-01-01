b18acb23bcff1345d4d35abdacd23d51dda4d1da
ManasviiJ <manasvij.mahi@gmail.com>
2025-01-02 01:06:32 +0530

    h

--- pages/progress.py
+++ pages/progress.py
@@ -1,7 +1,7 @@
 from streamlit import *
 
 page_link("pages/Habits.py",label="",icon="🔙")
-bar_chart(session_state.df,x="time of the day",y="number of hours",color="habits",stack=False)
+bar_chart(session_state.df,x="Time of the Day",y="Hours",color="Habit",stack=False)
 
 
 
