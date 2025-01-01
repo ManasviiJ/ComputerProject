5c8c5cf2111f37400aba0fd070c5d3021475e5a3
ManasviiJ <manasvij.mahi@gmail.com>
2025-01-01 13:38:54 +0530

    Update Habits.py

--- pages/Habits.py
+++ pages/Habits.py
@@ -9,18 +9,4 @@ col1,col2=columns(2)
 with col1:
     page_link("pages/progress.py",label="See your progress",icon="🚨")
     page_link("pages/addhabit.py",label="add a new habit",icon="🔥")
-    
-#Initializing session state to store habits as a dictionary
-if 'habits' not in session_state:
-    session_state.habits={}
-   
-#Entering new habits and displaying them
-new_habit=text_input('Enter you new habit', label_visibility='collapsed')
-days_to_follow=select_slider('How  many ***days*** do you wish to follow your new habit?', options=range(31))
-  
-if button('Add habit'):
-    session_state.habits[new_habit]=days_to_follow
-  
-subheader('Your new habits:')
-for habit, days in session_state.habits.items():
-    checkbox(f"{habit} for {days} days")
+
