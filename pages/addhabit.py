f5e9204cda01f9dafadd758a2177c395bd52a285
ManasviiJ <manasvij.mahi@gmail.com>
2025-01-01 22:13:02 +0530

    dcaca

--- pages/addhabit.py
+++ pages/addhabit.py
@@ -1,5 +1,6 @@
 from streamlit import *
 from streamlit import segmented_control
+import datetime
 
 page_link("pages/Habits.py",label="",icon="🔙")
 
@@ -14,30 +15,31 @@ new_habit=text_input('Enter you new habit', label_visibility='collapsed')
 
 options = ["Morning", "Afternoon", "Evening"]
 selection =segmented_control("Select a category", options, selection_mode="single")
+time = time_input("When will you begin this habit", value=None)
 
 if button('Add Habit'):
-    session_state.habits[selection]=[new_habit]
+    session_state.habits[selection]=[new_habit,time]
 
 col1,col2,col3=columns(3)
 with col1:
     subheader('Morning')
     for k,v in session_state.habits.items():
         if k=="Morning":
-            if checkbox(v[0]):
+            if checkbox(v[0],'at',v[1]):
                 session_state.habits.pop(k)
                 rerun()
 with col2:
     subheader("Afternoon")
     for k,v in session_state.habits.items():
         if k=="Afternoon":
-            if checkbox(v[0]):
+            if checkbox(v[0],'at',v[1]):
                 session_state.habits.pop(k)
                 rerun()
 with col3:
     subheader('Evening')
     for k,v in session_state.habits.items():
         if k=="Evening":
-            if checkbox(v[0]):
+            if checkbox(v[0],'at',v[1]):
                 session_state.habits.pop(k)
                 rerun()
 
