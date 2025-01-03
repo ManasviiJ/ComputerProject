920b5e58ed76f95f2e832cb7f542637747b40388
ManasviiJ <manasvij.mahi@gmail.com>
2025-01-04 00:12:03 +0530

    k

--- pages/progress.py
+++ pages/progress.py
@@ -2,7 +2,7 @@ from streamlit import *
 
 page_link("pages/Habits.py",label="",icon="🔙")
 toast('Hello there! Try adding completing habits to see your progress')
-
+title(":Green[See you progress]")
 bar_chart(session_state.df,x="Time of Day",y="Hours",color="Habit",stack=False)
 
 
